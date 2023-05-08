1a8acabf99ba045c8474a00eccedd061dd3091d9
Andy Richardson <arichardson@sdzwa.org>
2023-05-08 10:55:41 -0700

    add subspecies data

--- src/script1.sql
+++ src/script1.sql
@@ -1,7 +1,7 @@
 /* find all species level data */
 select tu.*
 from taxonomic_units tu
-where tu.rank_id in ('220') --species, '230' --subspecies
+where tu.rank_id in ('220','230') --species,subspecies
     --and tu.kingtom_id = '5' --animalia
 order by tu.tsn
 ;
--- src/script2.sql
+++ src/script2.sql
@@ -5,6 +5,6 @@ select tu.tsn
     ,tu.rank_id
 from taxonomic_units tu
 where tu.rank_id not in ('220','230') --species, subspecies
-    and tu.kingdom_id = '5' --animals, remove if desired
+    --and tu.kingdom_id = '5' --animals, remove if desired
 order by tu.tsn
 ;
