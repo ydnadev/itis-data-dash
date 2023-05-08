849d74ca670f1d7e29bdb7d77a48dcdf28163e97
Andy Richardson <arichardson@sdzwa.org>
2023-05-08 11:58:23 -0700

    update sql queries

--- src/script1-subspecies.sql
+++ src/script1-subspecies.sql
@@ -1,7 +1,7 @@
 /* find all species level data */
 select tu.*
 from taxonomic_units tu
-where tu.rank_id in ('220') --species
+where tu.rank_id in ('230') --subspecies
     --and tu.kingtom_id = '5' --animalia
 order by tu.tsn
 ;
---
+++ src/script2-subspecies.sql
@@ -0,0 +1,10 @@
+/* export all animal taxa */
+select tu.tsn
+    ,tu.unit_name1
+    ,tu.parent_tsn
+    ,tu.rank_id
+from taxonomic_units tu
+where tu.rank_id not in ('230') --subspecies
+    --and tu.kingdom_id = '5' --animals, remove if desired
+order by tu.tsn
+;
