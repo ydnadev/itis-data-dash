48f984af37a57cd15aa71c280a4c3861f823a027
Andy Richardson <arichardson@sdzwa.org>
2023-06-06 09:08:11 -0700

    add custom css

--- itis_data_dash.py
+++ itis_data_dash.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import pyarrow.parquet as pq
 import streamlit as st
 from fastparquet import ParquetFile
+
+
+# CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Streamlit config
 st.set_page_config(
     page_title = 'ITIS Lookup',
     layout = 'wide',
 )
+local_css("css/streamlit.css")
 pd.set_option('display.max_rows',None)
 
 # CSV convert def
