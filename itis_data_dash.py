b903990dfb88878c6890ab60f9a22c30369b484b
Andy Richardson <arichardson@sdzwa.org>
2024-01-09 08:30:48 -0800

    Update data for 08-Dec-23

--- itis_data_dash.py
+++ itis_data_dash.py
@@ -40,7 +40,7 @@ st.header("ITIS Taxa Lookup")
 st.write(
     "Data from Integrated Taxonomic Information System (ITIS) - https://www.itis.gov/"
 )
-st.write("data load date: :blue[17-Nov-2023]")
+st.write("data load date: :blue[08-Dec-2023]")
 st.write("TSN -- Taxonomic Serial Number")
 
 # Get data from parquet file for vernacular names
